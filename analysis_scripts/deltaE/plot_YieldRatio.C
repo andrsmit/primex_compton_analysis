d790fd9cd25b6f399a2f66b0127d9d20a4fcb7f2
andrsmit <andrsmit@jlab.org>
2020-12-02 14:12:11 -0500

    Add pdfs to show systematics related to deltaE cut width.

--- analysis_scripts/deltaE/plot_YieldRatio.C
+++ analysis_scripts/deltaE/plot_YieldRatio.C
@@ -2,12 +2,11 @@
 void plot_YieldRatio()
 {
 	
+	int tag_sys = 0;
 	
-	int tag_sys =   0;
-	int counter = 205;
-	
-	if( counter < 1 || counter > 274 ) return;
-	if( tag_sys ==1 && counter > 102 ) return;
+	int MAX_COUNTERS;
+	if( tag_sys==0 ) MAX_COUNTERS=274;
+	else             MAX_COUNTERS=102;
 	
 	
 	vector<int> sigma_vec = {10, 20, 30, 35, 40, 45, 50, 55, 60, 70, 80};
@@ -64,117 +63,173 @@ void plot_YieldRatio()
 	
 	
 	
-	double *sigmas       = new double[n_ens];
-	double *zeros        = new double[n_ens];
-	double *yield_ratio  = new double[n_ens];
-	double *yield_ratioE = new double[n_ens];
-	double *acc_ratio    = new double[n_ens];
-	double *acc_ratioE   = new double[n_ens];
 	
-	double *cs  = new double[n_ens];
-	double *csE = new double[n_ens];
-	
-	for( int ie = 0; ie < n_ens; ie++ ) {
-		
-		sigmas[ie]  = (double)sigma_vec[ie];
-		zeros[ie]   = 0.;
-		
-		double loc_yield_ratio, loc_yield_ratioE;
-		double loc_acc_ratio, loc_acc_ratioE;
-		double loc_cs, loc_csE;
-		
-		if( !tag_sys ) {
-			loc_yield_ratio  = tagh_yield[ie][counter-1] / tagh_yield[0][counter-1];
-			loc_yield_ratioE = sqrt( 
-				pow( tagh_yieldE[ie][counter-1]/tagh_yield[0][counter-1], 2.0) + 
-				pow( tagh_yield[ie][counter-1]*tagh_yieldE[0][counter-1]
-				/(tagh_yield[0][counter-1]*tagh_yield[0][counter-1]), 2.0) );
-			loc_acc_ratio    = tagh_acc[ie][counter-1] / tagh_acc[0][counter-1];
-			loc_acc_ratioE   = sqrt( 
-				pow( tagh_accE[ie][counter-1]/tagh_acc[0][counter-1], 2.0) + 
-				pow( tagh_acc[ie][counter-1]*tagh_accE[0][counter-1]
-				/(tagh_acc[0][counter-1]*tagh_acc[0][counter-1]), 2.0) );
-			loc_cs  = tagh_cs[ie][counter-1];
-			loc_csE = tagh_csE[ie][counter-1];
-		} else {
-			loc_yield_ratio  = tagm_yield[ie][counter-1] / tagm_yield[0][counter-1];
-			loc_yield_ratioE = sqrt( 
-				pow( tagm_yieldE[ie][counter-1]/tagm_yield[0][counter-1], 2.0) + 
-				pow( tagm_yield[ie][counter-1]*tagm_yieldE[0][counter-1]
-				/(tagm_yield[0][counter-1]*tagm_yield[0][counter-1]), 2.0) );
-			loc_acc_ratio    = tagm_acc[ie][counter-1] / tagm_acc[0][counter-1];
-			loc_acc_ratioE   = sqrt( 
-				pow( tagm_accE[ie][counter-1]/tagm_acc[0][counter-1], 2.0) + 
-				pow( tagm_acc[ie][counter-1]*tagm_accE[0][counter-1]
-				/(tagm_acc[0][counter-1]*tagm_acc[0][counter-1]), 2.0) );
-			loc_cs  = tagm_cs[ie][counter-1];
-			loc_csE = tagm_csE[ie][counter-1];
-		}
-		
-		yield_ratio[ie]  = loc_yield_ratio;
-		yield_ratioE[ie] = loc_yield_ratioE;
-		
-		acc_ratio[ie]    = loc_acc_ratio;
-		acc_ratioE[ie]   = loc_acc_ratioE;
-		
-		cs[ie]  = loc_cs;
-		csE[ie] = loc_csE;
-		
-		
-	}
-	
-	
-	TGraphErrors *gYieldRatio = new TGraphErrors( n_ens, sigmas, yield_ratio, 
-		zeros, yield_ratioE );
-	gYieldRatio->GetXaxis()->SetTitle( "#DeltaE Cut Width [#sigma]" );
-	gYieldRatio->GetYaxis()->SetTitle( "Relative Yield" );
-	if( !tag_sys ) {
-		gYieldRatio->SetTitle( Form("TAGH Counter %d (E_{#gamma} = %.4f GeV)", 
-			counter, tagh_en[counter-1]) );
-	} else {
-		gYieldRatio->SetTitle( Form("TAGM Counter %d (E_{#gamma} = %.4f GeV)", 
-			counter, tagm_en[counter-1]) );
-	}
-	gYieldRatio->SetMarkerStyle(23);
-	gYieldRatio->SetMarkerColor(kBlue);
-	
-	TGraphErrors *gAccRatio = new TGraphErrors( n_ens, sigmas, acc_ratio, 
-		zeros, acc_ratioE );
-	gAccRatio->GetXaxis()->SetTitle( "#DeltaE Cut Width [#sigma]" );
-	gAccRatio->GetYaxis()->SetTitle( "Relative Yield" );
-	if( !tag_sys ) {
-		gAccRatio->SetTitle( Form("TAGH Counter %d (E_{#gamma} = %.4f GeV)", 
-			counter, tagh_en[counter-1]) );
-	} else {
-		gAccRatio->SetTitle( Form("TAGM Counter %d (E_{#gamma} = %.4f GeV)", 
-			counter, tagm_en[counter-1]) );
-	}
-	gAccRatio->SetMarkerStyle(23);
-	gAccRatio->SetMarkerColor(kRed);
-	
-	TCanvas *canvas = new TCanvas( "canvas", "canvas", 1000, 600 );
-	canvas->SetTickx(); canvas->SetTicky();
-	gYieldRatio->Draw("AP");
-	gAccRatio->Draw("P same");
-	
-	
-	
-	TGraphErrors *gCS = new TGraphErrors( n_ens, sigmas, cs, zeros, csE );
-	gCS->GetXaxis()->SetTitle( "#DeltaE Cut Width [#sigma]" );
-	gCS->GetYaxis()->SetTitle( "Cross Section [mb / electron]" );
-	if( !tag_sys ) {
-		gCS->SetTitle( Form("TAGH Counter %d (E_{#gamma} = %.4f GeV)", 
-			counter, tagh_en[counter-1]) );
-	} else {
-		gCS->SetTitle( Form("TAGM Counter %d (E_{#gamma} = %.4f GeV)", 
-			counter, tagm_en[counter-1]) );
-	}
-	gCS->SetMarkerStyle(23);
-	gCS->SetMarkerColor(kBlue);
+	TCanvas *canvas1 = new TCanvas( "canvas1", "canvas1", 1000, 600 );
+	canvas1->SetTickx(); canvas1->SetTicky();
 	
 	TCanvas *canvas2 = new TCanvas( "canvas2", "canvas2", 1000, 600 );
 	canvas2->SetTickx(); canvas2->SetTicky();
-	gCS->Draw("AP");
+	
+	
+	
+	int loc_counter = 0;
+	
+	for( int ic = 1; ic < MAX_COUNTERS; ic++ ) 
+	{
+		
+		
+		if( tag_sys==0 ) {
+			if( tagh_yield[0][ic-1] <= 0. ) continue;
+		} else {
+			if( tagm_yield[0][ic-1] <= 0. ) continue;
+		}
+		
+		
+		if( tag_sys==0 ) cout << "TAGH Counter " << ic << endl;
+		else             cout << "TAGM Counter " << ic << endl;
+		
+		
+		double *sigmas       = new double[n_ens];
+		double *zeros        = new double[n_ens];
+		double *yield_ratio  = new double[n_ens];
+		double *yield_ratioE = new double[n_ens];
+		double *acc_ratio    = new double[n_ens];
+		double *acc_ratioE   = new double[n_ens];
+		
+		double *cs  = new double[n_ens];
+		double *csE = new double[n_ens];
+		
+		for( int ie = 0; ie < n_ens; ie++ ) {
+			
+			sigmas[ie]      = 0.01 * (double)sigma_vec[ie];
+			zeros[ie]       = 0.;
+			
+			double loc_yield_ratio, loc_yield_ratioE;
+			double loc_acc_ratio, loc_acc_ratioE;
+			double loc_cs, loc_csE;
+			
+			if( !tag_sys ) {
+				loc_yield_ratio  = tagh_yield[ie][ic-1] / tagh_yield[0][ic-1];
+				loc_yield_ratioE = sqrt( 
+					pow( tagh_yieldE[ie][ic-1]/tagh_yield[0][ic-1], 2.0) + 
+					pow( tagh_yield[ie][ic-1]*tagh_yieldE[0][ic-1]
+					/(tagh_yield[0][ic-1]*tagh_yield[0][ic-1]), 2.0) );
+				loc_acc_ratio    = tagh_acc[ie][ic-1] / tagh_acc[0][ic-1];
+				loc_acc_ratioE   = sqrt( 
+					pow( tagh_accE[ie][ic-1]/tagh_acc[0][ic-1], 2.0) + 
+					pow( tagh_acc[ie][ic-1]*tagh_accE[0][ic-1]
+					/(tagh_acc[0][ic-1]*tagh_acc[0][ic-1]), 2.0) );
+				loc_cs  = tagh_cs[ie][ic-1];
+				loc_csE = tagh_csE[ie][ic-1];
+			} else {
+				loc_yield_ratio  = tagm_yield[ie][ic-1] / tagm_yield[0][ic-1];
+				loc_yield_ratioE = sqrt( 
+					pow( tagm_yieldE[ie][ic-1]/tagm_yield[0][ic-1], 2.0) + 
+					pow( tagm_yield[ie][ic-1]*tagm_yieldE[0][ic-1]
+					/(tagm_yield[0][ic-1]*tagm_yield[0][ic-1]), 2.0) );
+				loc_acc_ratio    = tagm_acc[ie][ic-1] / tagm_acc[0][ic-1];
+				loc_acc_ratioE   = sqrt( 
+					pow( tagm_accE[ie][ic-1]/tagm_acc[0][ic-1], 2.0) + 
+					pow( tagm_acc[ie][ic-1]*tagm_accE[0][ic-1]
+					/(tagm_acc[0][ic-1]*tagm_acc[0][ic-1]), 2.0) );
+				loc_cs  = tagm_cs[ie][ic-1];
+				loc_csE = tagm_csE[ie][ic-1];
+			}
+			
+			yield_ratio[ie]  = loc_yield_ratio;
+			yield_ratioE[ie] = loc_yield_ratioE;
+			
+			acc_ratio[ie]    = loc_acc_ratio;
+			acc_ratioE[ie]   = loc_acc_ratioE;
+			
+			cs[ie]  = loc_cs;
+			csE[ie] = loc_csE;
+			
+			
+		}
+		
+		
+		TGraphErrors *gYieldRatio = new TGraphErrors( n_ens, sigmas, yield_ratio, 
+			zeros, yield_ratioE );
+		gYieldRatio->GetXaxis()->SetTitle( "#DeltaE Cut Width [#sigma]" );
+		gYieldRatio->GetYaxis()->SetTitle( "Relative Yield" );
+		if( !tag_sys ) {
+			gYieldRatio->SetTitle( Form("TAGH Counter %d (E_{#gamma} = %.4f GeV)", 
+				ic, tagh_en[ic-1]) );
+		} else {
+			gYieldRatio->SetTitle( Form("TAGM Counter %d (E_{#gamma} = %.4f GeV)", 
+				ic, tagm_en[ic-1]) );
+		}
+		gYieldRatio->SetMarkerStyle(23);
+		gYieldRatio->SetMarkerColor(kBlue);
+		
+		TGraphErrors *gAccRatio = new TGraphErrors( n_ens, sigmas, acc_ratio, 
+			zeros, acc_ratioE );
+		gAccRatio->GetXaxis()->SetTitle( "#DeltaE Cut Width [#sigma]" );
+		gAccRatio->GetYaxis()->SetTitle( "Relative Yield" );
+		if( !tag_sys ) {
+			gAccRatio->SetTitle( Form("TAGH Counter %d (E_{#gamma} = %.4f GeV)", 
+				ic, tagh_en[ic-1]) );
+		} else {
+			gAccRatio->SetTitle( Form("TAGM Counter %d (E_{#gamma} = %.4f GeV)", 
+				ic, tagm_en[ic-1]) );
+		}
+		gAccRatio->SetMarkerStyle(23);
+		gAccRatio->SetMarkerColor(kRed);
+		
+		canvas1->cd();
+		gYieldRatio->Draw("AP");
+		gAccRatio->Draw("P same");
+		
+		
+		
+		TGraphErrors *gCS = new TGraphErrors( n_ens, sigmas, cs, zeros, csE );
+		gCS->GetXaxis()->SetTitle( "#DeltaE Cut Width [#sigma]" );
+		gCS->GetYaxis()->SetTitle( "Cross Section [mb / electron]" );
+		if( !tag_sys ) {
+			gCS->SetTitle( Form("TAGH Counter %d (E_{#gamma} = %.4f GeV)", 
+				ic, tagh_en[ic-1]) );
+		} else {
+			gCS->SetTitle( Form("TAGM Counter %d (E_{#gamma} = %.4f GeV)", 
+				ic, tagm_en[ic-1]) );
+		}
+		gCS->SetMarkerStyle(23);
+		gCS->SetMarkerColor(kBlue);
+		
+		canvas2->cd();
+		gCS->Draw("AP");
+		
+		
+		
+		if( loc_counter==0 ) {
+			if( tag_sys==0 ) {
+				canvas1->Print( "tagh_deltaE_yieldRatio.pdf(",   "pdf" );
+				canvas2->Print( "tagh_deltaE_crossSection.pdf(", "pdf" );
+			} else {
+				canvas1->Print( "tagm_deltaE_yieldRatio.pdf(",   "pdf" );
+				canvas2->Print( "tagm_deltaE_crossSection.pdf(",  "pdf" );
+			}
+		} else {
+			if( tag_sys==0 ) {
+				canvas1->Print( "tagh_deltaE_yieldRatio.pdf",    "pdf" );
+				canvas2->Print( "tagh_deltaE_crossSection.pdf",  "pdf" );
+			} else {
+				canvas1->Print( "tagm_deltaE_yieldRatio.pdf",    "pdf" );
+				canvas2->Print( "tagm_deltaE_crossSection.pdf",  "pdf" );
+			}
+		}
+		
+		loc_counter++;
+		
+	}
+	
+	if( tag_sys==0 ) {
+		canvas1->Print( "tagh_deltaE_yieldRatio.pdf)",   "pdf" );
+		canvas2->Print( "tagh_deltaE_crossSection.pdf)", "pdf" );
+	} else {
+		canvas1->Print( "tagm_deltaE_yieldRatio.pdf)",   "pdf" );
+		canvas2->Print( "tagm_deltaE_crossSection.pdf)", "pdf" );
+	}
 	
 	return;
 }
